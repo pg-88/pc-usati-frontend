fdd887ea469e4b448f3c3af48f4b72404760e1ad
Pietro Grigolo <grigolopi@gmail.com>
2023-06-21 08:06:30 +0200

    modificato array dati

--- src/Shop.jsx
+++ src/Shop.jsx
@@ -4,28 +4,22 @@ import Header from "./components/Header";
 import Section from "./components/Section";
 import ProdList from "./components/ProdList";
 
-import FAKE_DB_DATA from "./utils/Response";
+import AVAILABLE_PROD  from "./utils/Response";
 
 
 export default function Shop(){
     const [category, setCategory] = useState('');
-    const categories = FAKE_DB_DATA.map(cat => Object.keys(cat));
-    const prod_family = categories.map(en => 
-        <Section 
-            categoria={en} 
-            onClick={e => {
-                console.log('aggiorno la categoria con useState:', e.target.id);
-                setCategory(e.target.id);
-                console.log(category);
-                console.log('selected category products', FAKE_DB_DATA.filter(cat => Object.keys(cat)[0] === category));
-            }}
-            key={en.toString()}/>);
-            console.log(prod_family)
-
+    let catList = [];
+    AVAILABLE_PROD.forEach(p => {
+        catList.includes(p.type) ? 
+        console.log(`category ${p.type} alredy present`) :
+        catList.push(p.type);
+        });
+    console.log('categorie prodotti: ', catList);
     return(
         <div>
             <Header />
-            {prod_family}
+
             <ProdList />
 
         </div>
--- src/utils/Response.js
+++ src/utils/Response.js
@@ -1,183 +1,204 @@
 //creo a manazza un array che potrebbe essere il ritorno di una chiamata API
-const FAKE_DB_DATA = 
+const AVAILABLE_PROD = 
 [
     {
-        smartphone: 
-        [ 
-            {
-                brand: 'mela',
-                model: 'Tphone69',
-                id: 'dsjfòadòiosjf4454kjfaklòsdfj',
-                specs: 
-                {
-                    ram: '6GB',
-                    cpu: 'draghetto 2,78GHz',
-                    memory: '128GB',
-                    battery: '5600mAh',
-                    memExpansion: false
-                }
-            },
-            {
-                brand: 'br0ndi',
-                model: 'b88',
-                id: 'dgfhweicm5ib7887skk',
-                specs: 
-                {
-                    ram: '2GB',
-                    cpu: 'mediaTroll 2,69GHz',
-                    memory: '32GB',
-                    battery: '3800 mAh',
-                    memExpansion: true
-                }
-            },
-            {
-                brand: 'br0ndi',
-                model: 'b88',
-                id: '334990iieu',
-                specs: 
-                {
-                    ram: '2GB',
-                    cpu: 'mediaTroll 2,69GHz',
-                    memory: '32GB',
-                    battery: '3800 mAh',
-                    memExpansion: true
-                }
-            },
-            {
-                brand: 'br0ndi',
-                model: 'b88',
-                id: '22ppo3or59e',
-                specs: 
-                {
-                    ram: '2GB',
-                    cpu: 'mediaTroll 2,69GHz',
-                    memory: '32GB',
-                    battery: '3800 mAh',
-                    memExpansion: true
-                }
-            },
-        ]
-    },
-    {
-        laptop:
-        [
-            {
-                brand: 'Acero',
-                model: 'Aspirato 420',
-                id: 'assp90908000',
-                specs:
-                {
-                    ram: '12GB DDR3',
-                    memory: 'SSD 256GB',
-                    so: 'Winzozs8.1',
-                    cpu: 'Int3l 3 5th gen',
-                    gpu: 'N/A',
-                },
-            },
-            {
-                brand: 'Asthem',
-                model: 'avgnn54i998-69',
-                id: '3322233322nnn3nffiie0xoow',
-                specs:
-                {
-                    ram: '8GB DDR4',
-                    memory: 'SSD 512GB',
-                    so: 'Winzozs10',
-                    cpu: 'Int3l 7 4th gen',
-                    gpu: 'invidiosa geForza 420M',
-                },
-            },
-            {
-                brand: 'Asthem',
-                model: 'avgnn54i998-69',
-                id: '3322233322nnn3nffiie0xo',
-                specs:
-                {
-                    ram: '8GB DDR4',
-                    memory: 'SSD 512GB',
-                    so: 'Winzozs10',
-                    cpu: 'Int3l 7 4th gen',
-                    gpu: 'invidiosa geForza 420M',
-                },
-            },
-            {
-                brand: 'Asthem',
-                model: 'avgnn54i998-69',
-                id: '3322233322nnn3nffiie0xoo',
-                specs:
-                {
-                    ram: '8GB DDR4',
-                    memory: 'SSD 512GB',
-                    so: 'Winzozs10',
-                    cpu: 'Int3l 7 4th gen',
-                    gpu: 'invidiosa geForza 420M',
-                },
-            }
+        id: 'dsjfòadòiosjf4454kjfaklòsdfj',
+        type: 'smartphone',
+        brand: 'mela',
+        model: 'Tphone69',
+        price: 320,
+        promo: false,
+        specs:
 
-        ]
+        {
+            ram: '6GB',
+            cpu: 'draghetto 2,78GHz',
+            memory: '128GB',
+            battery: '5600mAh',
+            memExpansion: false
+        }
     },
     {
-        tablet: 
-        [ 
-            {
-                brand: 'mela',
-                model: 'Tphone69',
-                id: 'dsjfòadòiosjf4454kjfaklòsdfj',
-                specs: 
-                {
-                    ram: '6GB',
-                    cpu: 'draghetto 2,78GHz',
-                    memory: '128GB',
-                    battery: '5600mAh',
-                    memExpansion: false
-                }
-            },
-            {
-                brand: 'br0ndi',
-                model: 'b88',
-                id: 'dgfhweicm5ib7887skk',
-                specs: 
-                {
-                    ram: '2GB',
-                    cpu: 'mediaTroll 2,69GHz',
-                    memory: '32GB',
-                    battery: '3800 mAh',
-                    memExpansion: true
-                }
-            },
-        ]
+        id: 'dgfhweicm5ib7887skk',
+        type: 'smartphone',
+        brand: 'br0ndi',
+        model: 'b88',
+        price: 320,
+        promo: true,
+        discount: 5,
+        specs: 
+        {
+            ram: '2GB',
+            cpu: 'mediaTroll 2,69GHz',
+            memory: '32GB',
+            battery: '3800 mAh',
+            memExpansion: true
+        }
     },
     {
-        chromebook:
-        [
-            {
-                brand: 'Acero',
-                model: 'Aspirato 420',
-                id: 'assp90908000',
-                specs:
-                {
-                    ram: '12GB DDR3',
-                    memory: 'SSD 256GB',
-                    so: 'Winzozs8.1',
-                    cpu: 'Int3l 3 5th gen',
-                    gpu: 'N/A',
-                },
-            },
-            {
-                brand: 'Asthem',
-                model: 'avgnn54i998-69',
-                id: '3322233322nnn3nffiie0xoow',
-                specs:
-                {
-                    ram: '8GB DDR4',
-                    memory: 'SSD 512GB',
-                    so: 'Winzozs10',
-                    cpu: 'Int3l 7 4th gen',
-                    gpu: 'invidiosa geForza 420M',
-                },
-            }
-        ]
+        id: '334990iieu',
+        type: 'smartphone',
+        brand: 'br0ndi',
+        model: 'b88',
+        price: 99,
+        promo: false,
+        specs: 
+        {
+            ram: '2GB',
+            cpu: 'mediaTroll 2,69GHz',
+            memory: '32GB',
+            battery: '3800 mAh',
+            memExpansion: true
+        }
+    },
+    {
+        id: '22ppo3or59e',
+        type: 'smartphone',
+        brand: 'br0ndi',
+        model: 'b88',
+        price: 99.99,
+        promo: false,
+        specs: 
+        {
+            ram: '2GB',
+            cpu: 'mediaTroll 2,69GHz',
+            memory: '32GB',
+            battery: '3800 mAh',
+            memExpansion: true
+        }
+    },
+    {
+        id: 'assp90908000',
+        type: 'laptop',
+        brand: 'Acero',
+        model: 'Aspirato 420',
+        price: 299.99,
+        promo: false,
+        specs:
+        {
+            ram: '12GB DDR3',
+            memory: 'SSD 256GB',
+            so: 'Winzozs8.1',
+            cpu: 'Int3l 3 5th gen',
+            gpu: 'N/A',
+        },
+    },
+    {
+        id: 'assp90908000',
+        type: 'laptop',
+        brand: 'Acero',
+        model: 'Aspirato 420',
+        price: 299.99,
+        promo: true,
+        discount: 20,
+        specs:
+        {
+            ram: '12GB DDR3',
+            memory: 'SSD 256GB',
+            so: 'Winzozs8.1',
+            cpu: 'Int3l 3 5th gen',
+            gpu: 'N/A',
+        },
+    },
+    {
+        id: '3322233322nnn3nffiie0xoow',
+        type: 'laptop',
+        brand: 'Asthem',
+        model: 'avgnn54i998-69',
+        price: 299.99,
+        promo: false,
+        specs:
+        {
+            ram: '8GB DDR4',
+            memory: 'SSD 512GB',
+            so: 'Winzozs10',
+            cpu: 'Int3l 7 4th gen',
+            gpu: 'invidiosa geForza 420M',
+        },
+    },
+    {
+        id: '3322233322nnn3nffiie0xo',
+        type: 'laptop',
+        brand: 'Asthem',
+        model: 'avgnn54i998-69',
+        price: 699.99,
+        promo: true,
+        discount: 10,
+        specs:
+        {
+            ram: '8GB DDR4',
+            memory: 'SSD 512GB',
+            so: 'Winzozs10',
+            cpu: 'Int3l 7 4th gen',
+            gpu: 'invidiosa geForza 420M',
+        },
+    },
+    {
+        id: '3322233322nnn3nffiie0xo',
+        type: 'laptop',
+        brand: 'Asthem',
+        model: 'avgnn54i998-69',
+        price: 670,
+        promo: false,
+        specs:
+        {
+            ram: '8GB DDR4',
+            memory: 'SSD 512GB',
+            so: 'Winzozs10',
+            cpu: 'Int3l 7 4th gen',
+            gpu: 'invidiosa geForza 420M',
+        },
+    },
+    {
+        id: 'dsjfòadòiosjf4454kjfaklòsdfj',
+        type: 'tablet',
+        brand: 'mela',
+        model: 'Tphone69',
+        price: 320,
+        promo: false,
+        specs: 
+        {
+            ram: '6GB',
+            cpu: 'draghetto 2,78GHz',
+            memory: '128GB',
+            battery: '5600mAh',
+            memExpansion: false
+        }
+    },
+    {
+        id: 'dgfhweicm5ib7887skk',
+        type: 'tablet',
+        brand: 'br0ndi',
+        model: 'b88',
+        price: 320,
+        promo: true,
+        discount: 12,
+        specs: 
+        {
+            ram: '2GB',
+            cpu: 'mediaTroll 2,69GHz',
+            memory: '32GB',
+            battery: '3800 mAh',
+            memExpansion: true
+        }
+    },
+    {
+        id: 'ch4335ddggr',
+        type: 'chromebook',
+        brand: 'google',
+        model: 'ch666',
+        price: 320,
+        promo: false,
+        specs: 
+        {
+            ram: '6GB',
+            cpu: 'draghetto 2,78GHz',
+            memory: '128GB',
+            battery: '5600mAh',
+            memExpansion: false
+        }
+        
     }
-];
+]
 
-export default FAKE_DB_DATA; 
+export default AVAILABLE_PROD; 

5293340a470b5eb404651f0758cd5b25ed9e7118
Pietro Grigolo <grigolopi@gmail.com>
2023-07-05 10:56:01 +0200

    card e riorganizzate funzioni

--- src/init.js
+++ src/init.js
@@ -8,42 +8,19 @@ export function startApp(){
      * menù richiamato da selCategoria
      */
 
+    //manipolazione head
     document.title = "Negozio online - Shop";
-    document.body.appendChild(header());
-    let top = document.createElement('header');
-    top.id = "page-header";
-
+    
+    //estrapolo le categorie
     let categorie = []
     console.log("app avviata");
     AVAILABLE_PROD.forEach(element => {
         console.log("elemento",element.type);
         !categorie.includes(element.type) ? categorie.push(element.type) : undefined;
     });
-
-    //genero il main (unico ma verrà poi manipolato)
-    let mainEl = document.createElement('main');
-    //elementi per il main
-    let txt = document.createTextNode(
-        `Com'è bello usare i PC vecchi e fa bene all'ambiente!\n
-        inserire qui il testo green washing che fa vendere tanto di questi tempi:
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-        incididunt ut labore et dolore magna aliqua. Leo integer malesuada nunc vel risus
-        commodo viverra maecenas. In ornare quam viverra orci sagittis eu volutpat odio facilisis. 
-        Nisi quis eleifend quam adipiscing vitae. Sollicitudin aliquam ultrices sagittis orci a scelerisque
-        purus semper. Congue mauris rhoncus aenean vel elit scelerisque mauris. 
-        Scelerisque viverra mauris in aliquam sem fringilla ut.   
-        Integer malesuada nunc vel risus commodo viverra maecenas accumsan lacus. 
-        Id diam maecenas ultricies mi. Sapien et ligula ullamcorper malesuada proin libero nunc.`
-        );
     
-    let video = document.createElement('iframe');
-    video.width = 400;
-    video.height = 240;
-    video.src="https://www.youtube.com/embed/12zfutD9JxQ";
-    video.title = "Old PC Commercial"
-    video.allow = "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-    mainEl.append(txt, video);
-    document.body.lastElementChild.after(navbar(categorie), mainEl);
+    //inserisco elementi nel DOM con le apposite funzioni
+    document.body.append(header(), navbar(categorie), iniMain(), foot());
 }
 
 let header = () => {
@@ -74,6 +51,16 @@ let header = () => {
     return content;
 }
 
+let foot = () => {
+    /**genera il footer */
+    let pie = document.createElement('footer');
+    let indirizzo = document.createElement('address');
+    indirizzo.title = "Riuso e Riciclo dispositivi tecnologici";
+    indirizzo.append(document.createTextNode("Riuso e Riciclo dispositivi tecnologici"));
+    pie.append(indirizzo);
+    return pie;
+}
+
 let navbar = (arrCat) => {
     /**passato l'array con i nomi delle categgorie di prodotti genera in navbar
      * costituito da bottoni 
@@ -102,7 +89,35 @@ let navbar = (arrCat) => {
     element.append(list);
 
     return element;
-} 
+}
+
+let iniMain = () => {
+
+    //genero il main iniziale di default poi verrà manipolato
+    let mainEl = document.createElement('main');
+    //elementi per il main
+    let txt = document.createTextNode(
+        `Com'è bello usare i PC vecchi e fa bene all'ambiente!\n
+        inserire qui il testo green washing che fa vendere tanto di questi tempi:
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+        incididunt ut labore et dolore magna aliqua. Leo integer malesuada nunc vel risus
+        commodo viverra maecenas. In ornare quam viverra orci sagittis eu volutpat odio facilisis. 
+        Nisi quis eleifend quam adipiscing vitae. Sollicitudin aliquam ultrices sagittis orci a scelerisque
+        purus semper. Congue mauris rhoncus aenean vel elit scelerisque mauris. 
+        Scelerisque viverra mauris in aliquam sem fringilla ut.   
+        Integer malesuada nunc vel risus commodo viverra maecenas accumsan lacus. 
+        Id diam maecenas ultricies mi. Sapien et ligula ullamcorper malesuada proin libero nunc.`
+        );
+    //oggetto multimediale
+    let video = document.createElement('iframe');
+    video.width = 400;
+    video.height = 240;
+    video.src="https://www.youtube.com/embed/12zfutD9JxQ";
+    video.title = "Old PC Commercial"
+    video.allow = "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+    mainEl.append(txt, video);
+    return mainEl;
+}
 
 function clickCat(id){ 
     /**filtra categoria e passa a cards gli elementi da visualizzare */
@@ -110,15 +125,59 @@ function clickCat(id){
     let prods = AVAILABLE_PROD.filter((en) => en.type === id);
     console.log("prodotti trovati ", prods);
 
-    cards(prods);
+    showcase(prods);
 }
 
-let cards = (arrProd) => {
-    /**card sono anchor che stanno nel main
-     * prima di tutto svuota il main, quindi genera gli elementi necessari
-     * per ogni card, quindi ritortna il main popolato dalle card.
-     */
+let showcase = (arrProd) => {
+    /**svuota il main e lo ripopola con le card  */
+    //Lo svuota main:
+    let mainEl = document.querySelector('main');
+    mainEl.innerHTML = '';
+    mainEl.setAttribute('class', 'showcase');
+    let h = document.createElement('h3');
+    h.append(`Selezionato la categoria ${arrProd[0].type}`);
+    mainEl.append(h)
+    document.title = `Negozio online - ${arrProd[0].type}`;
+    //container per le card
+    let content = document.createElement('div');
+    content.setAttribute('class', 'grid-container');
+    //array per elementi delle card
+    let cardArr = [];
+
+    //ciclo per popolare cardArr
     arrProd.forEach(p => {
-        console.log(`Prodotto: ${p.type}, ${p.id}`);
-    });
+        console.log("card per il prodotto: ", p)
+        cardArr.push(card(p))
+    })
+
+    //inserimento DOM nel main
+    content.append(...cardArr);
+    mainEl.append(content);
+}
+
+let card = (article) => {
+    /**ritorna una card costruita per l'oggetto passato come parametro*/
+    //oggetto di base per le card
+    let card = document.createElement('div');
+    card.setAttribute('class', 'card-item');
+    card.setAttribute('id', `${article.id}`);
+    //lista caratteristiche
+    let list = document.createElement('ul');
+    const brand = document.createElement('li');
+    brand.append(`Marca: ${article.brand}`);
+    const model = document.createElement('li');
+    model.append(`Modello: ${article.model}`);
+    const price = document.createElement('li');
+    price.append(`Prezzo: ${article.price}`)
+    list.append(brand, model, price);
+    if(article.img !== undefined){
+        const pic = document.createElement('img');
+        pic.src = article.img;
+        pic.setAttribute('class', 'card-img');
+        card.append(pic);
+    }
+
+    card.append(list);
+    
+    return card;
 }
